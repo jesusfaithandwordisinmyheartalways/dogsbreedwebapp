3de14d7de91b60dd98ac8ad6208ba2f76ae4ab2e
Andrew Johnson <90214404+jesusfaithandwordisinmyheartalways@users.noreply.github.com>
2025-04-07 11:19:29 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -124,7 +124,7 @@ app.get('/', (req, res) => {
 
 
 
-const PORT = process.env.PORT || '3001'
+const PORT = process.env.PORT || 'https://dogstoreserver.onrender.com'
 app.listen(PORT, () => {
     console.log(`Server is on port ${PORT}`);
 });
