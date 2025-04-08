eb63021f8ed88a5d2d23b1fc0766b804465cc82b
andrewjohnson <andrewjohnson9393@gmail.com>
2025-04-07 21:59:17 -0500

    data

--- server/server.js
+++ server/server.js
@@ -120,7 +120,7 @@ app.get('/', (req, res) => {
 
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
