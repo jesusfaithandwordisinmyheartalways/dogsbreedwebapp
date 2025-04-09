e6ec2c6d78b2fbd9283d279cbf14d42a493a6713
Andrew Johnson <90214404+jesusfaithandwordisinmyheartalways@users.noreply.github.com>
2025-04-09 13:59:39 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 
 app.use(cors({
   credentials:true,
-  origin:'http://localhost:3000',
+  origin:'https://dogsmarketclient.onrender.com',
 }))
 
 app.use(helmet());
@@ -107,7 +107,7 @@ app.get('/', (req, res) => {
 
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
