90f864a0dcf0af9aec720ccc66ccbf8e5afd557c
andrewjohnson <andrewjohnson9393@gmail.com>
2025-04-10 11:06:56 -0500

    freshdata

--- server/server.js
+++ server/server.js
@@ -107,7 +107,7 @@ app.get('/', (req, res) => {
 
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
